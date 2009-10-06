eb1c96435dab200f524ef094b7f65c49671d6e76
visionmedia <tj@vision-media.ca>
2009-10-06 11:03:54 -0700

    Started formatters

--- lib/dm-pagination/pager.rb
+++ lib/dm-pagination/pager.rb
@@ -25,5 +25,33 @@ module DataMapper
       @per_page = options.delete :limit
       @current_page = options.delete :current_page
     end
+    
+    ##
+    # Render the pager with the given _formatter_
+    # which defaults to DataMapper::Pager::Formatter
+    
+    def to_html
+      Formatter.new(self).to_html
+    end
+    
+    #--
+    # Default formatter
+    #++
+    
+    class Formatter
+      
+      ##
+      # Pager instance.
+      
+      attr_reader :pager
+      
+      ##
+      # Initialize with _pager_.
+      
+      def initialize pager
+        @pager = pager
+      end
+    end
+    
   end
 end
