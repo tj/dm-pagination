1f183ead48e56c473b624c1a9049da85f994624b
visionmedia <tj@vision-media.ca>
2009-10-06 10:13:08 -0700

    Abstracting into seperate files

---
+++ lib/dm-pagination/pagination.rb
@@ -0,0 +1,34 @@
+
+module DataMapper
+  module Pagination
+    
+    ##
+    # Un-paged total.
+    
+    attr_accessor :total
+    
+    ##
+    # Page collection by the _current_ page number and _options_ provided.
+    #
+    # === Options
+    #
+    #   :per_page   Results per page; defaults to 6
+    #   :order      Defaults to [:id.desc]
+    #
+    # === Examples
+    #    
+    #   User.all.
+    #
+    
+    def page current = 1, options = {}
+      options, current = current, 1 if current.is_a? Hash
+      collection = new_collection scoped_query({
+        :limit => per_page = (options.delete(:per_page) || 6),
+        :offset => (current - 1) * per_page,
+        :order => [:id.desc]
+      }.merge(options))
+      collection.total = count options
+      collection
+    end
+  end
+end
---
+++ lib/dm-pagination/version.rb
@@ -0,0 +1,6 @@
+
+module DataMapper
+  module Pagination
+    VERSION = '0.0.1'
+  end
+end
