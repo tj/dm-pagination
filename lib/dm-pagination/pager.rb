5be8f3b67b2afe1f9042a6c8e6dbf3f607aa9905
visionmedia <tj@vision-media.ca>
2009-10-06 13:46:08 -0700

    Misc refactoring

--- lib/dm-pagination/pager.rb
+++ lib/dm-pagination/pager.rb
@@ -39,7 +39,7 @@ module DataMapper
       @total = options.delete :total
       @per_page = options.delete :limit
       @current_page = options.delete :current_page
-      @total_pages = (total.to_f / per_page.to_f).ceil
+      @total_pages = (total.to_f / per_page).ceil
       @next_page = current_page + 1 unless current_page >= total_pages
       @previous_page = current_page - 1 unless current_page <= 1
     end
@@ -49,21 +49,30 @@ module DataMapper
     #
     # === Options
     #
-    #   :size   Number of intermediate page number links to be shown
+    #   :size   Number of intermediate page number links to be shown; Defaults to 6
     #
     
     def to_html options = {}
       return if total_pages <= 0
-      raise ArgumentError, 'invalid :size; must be an odd number' if options[:size] && (options[:size] % 2) == 0
-      previous_link + '<ul class="pager">' + (1..options[:size] || total_pages).map { |n|
-        (n == current_page ? 
-          '<li class="active">%s</li>' : 
-            '<li>%s</li>') % link_to(n)
-      }.join("\n") + '</ul>' + next_link
+      # offset = active < size ? 0 : 
+      #     array.length - active < size ?
+      #       array.length - size : 
+      #         active - size / 2 - 1
+      # array[offset, size]
+      previous_link + '<ul class="pager">' + intermediate_links(options.delete(:size)) + '</ul>' + next_link
     end
     
     def link_to uri, contents = nil
       %(<a href="#{uri}">#{contents || uri}</a>)
+    end
+    
+    def intermediate_links size = 6
+      raise ArgumentError, 'invalid :size; must be an odd number' if size && size % 2 == 0
+      (1..size || total_pages).map { |n|
+        (n == current_page ? 
+          '<li class="active">%s</li>' : 
+            '<li>%s</li>') % link_to(n)
+      }.join("\n")
     end
     
     def previous_link
