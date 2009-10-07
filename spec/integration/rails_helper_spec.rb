ff136f808549b14c313d61ac63145a1af773ba41
visionmedia <tj@vision-media.ca>
2009-10-07 09:44:24 -0700

    Moved specs to /unit /integration

--- spec/integration/rails_helper_spec.rb
+++ spec/integration/rails_helper_spec.rb
@@ -1,5 +1,5 @@
 
-require File.dirname(__FILE__) + '/spec_helper'
+require File.dirname(__FILE__) + '/../spec_helper'
 
 describe DataMapper::Pagination::Helper::Rails do
   include DataMapper::Pagination::Helper::Rails
--- spec/unit/pagination_spec.rb
+++ spec/unit/pagination_spec.rb
@@ -1,5 +1,5 @@
 
-require File.dirname(__FILE__) + '/spec_helper'
+require File.dirname(__FILE__) + '/../spec_helper'
 
 describe DataMapper::Pagination do
   before(:each) { mock_items }
