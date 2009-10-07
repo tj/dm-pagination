19912b26f5bdd22eea17ffa5566d162f38055df9
visionmedia <tj@vision-media.ca>
2009-10-07 13:39:37 -0700

    Refactoring specs

--- spec/unit/pagination_spec.rb
+++ spec/unit/pagination_spec.rb
@@ -5,77 +5,99 @@ describe DataMapper::Pagination do
   before(:each) { mock_items }
   
   describe "#page" do
-    it "should default page to 1" do
-      Item.page.pager.current_page.should == 1
-    end
+    describe "given the page" do
+      it "return the appropriately sized collection" do
+        Item.page(1, :order => [:id.asc]).should == items(1, 6)
+        Item.page(2, :order => [:id.asc]).should == items(7, 12)
+        Item.page(3, :order => [:id.asc]).should == items(13, 18)
+      end
     
-    it "should treat a page < 1 as the first page" do
-      Item.page(0).pager.current_page.should == 1
-      Item.page(-1).pager.current_page.should == 1
-    end
-    
-    it "should treat nil as the first page" do
-      Item.page(nil).pager.current_page.should == 1
-    end
-    
-    it "should coerce strings to ints" do
-      Item.page('5').pager.current_page.should == 5
-      Item.page('5.0').pager.current_page.should == 5
-    end
-    
-    it "should treat a non-numeric string as the first page" do
-      Item.page('wahoo').pager.current_page.should == 1
-    end
-    
-    it "should default :per_page to 6" do
-      Item.page.length.should == 6
-    end
-    
-    it "should default :order to [:id.desc]" do
-      Item.page.should == items(15, 20).reverse
-    end
-    
-    it "should raise an error when page does not respond to #to_i" do
-      lambda { Item.page(true) }.should raise_error
-    end
-    
-    it "should raise an error when :page does not respond to #to_i" do
-      lambda { Item.page(:page => true) }.should raise_error
-      lambda { Item.page('page' => true) }.should raise_error
-    end
-    
-    it "should allow :page as a hash param" do
-      Item.page(:page => 2, :order => [:id.asc]).should == items(7, 12)
-      Item.page(:page => 2, :order => [:id.asc]).pager.current_page.should == 2
-    end
-    
-    it "should both keys from indifferent hashes when using :page" do
-      Item.page(:page => '2', 'page' => '2', :order => [:id.asc]).should == items(7, 12)
-      Item.page(:page => '2', 'page' => '2', :order => [:id.asc]).pager.current_page.should == 2
-    end
-    
-    it "should allow a hash of options as the first parameter" do
-      Item.page(:order => [:id.asc]).should == items(1, 6)
+      describe "number" do
+        it "should default to the first page" do
+          Item.page.pager.current_page.should == 1
+        end
+      end
+      
+      describe "nil" do
+        it "should default to the first page" do
+          Item.page(nil).pager.current_page.should == 1
+        end
+      end
+      
+      describe "number below 1" do
+        it "should default to the first page" do
+          Item.page(0).pager.current_page.should == 1
+          Item.page(-1).pager.current_page.should == 1
+        end
+      end
+      
+      describe "string" do
+        it "should be coerced to ints" do
+          Item.page('5').pager.current_page.should == 5
+          Item.page('5.0').pager.current_page.should == 5
+          Item.page('-1').pager.current_page.should == 1
+        end
+        
+        describe "which is not numeric" do
+          it "should default to the first page" do
+            Item.page('wahoo').pager.current_page.should == 1
+          end
+        end
+      end
+      
+      describe "of an arbitrary object" do
+        it "should raise an error unless responding to #to_i" do
+          lambda { Item.page(true) }.should raise_error
+        end
+      end
     end
 
-    it "should allow a hash of options as the second parameter" do
-      Item.page(1, :order => [:id.asc]).should == items(1, 6)
+    describe "options hash" do
+      it "should be accepted as first param" do
+        Item.page(:page => 2, :order => [:id.asc]).pager.current_page.should == 2
+      end
+      
+      it "should be accepted as second param" do
+        Item.page(3, :page => 2, :order => [:id.asc]).pager.current_page.should == 3
+      end
     end
-
-    it "should allow a page number to be passed as the first parameter" do
-      Item.page(1, :order => [:id.asc]).should == items(1, 6)
-      Item.page(2, :order => [:id.asc]).should == items(7, 12)
-      Item.page(3, :order => [:id.asc]).should == items(13, 18)
-    end
-
-    it "should allow :per_page to be overriden" do
-      Item.page(1, :per_page => 3, :order => [:id.asc]).should == items(1, 3)
-      Item.page(2, :per_page => 3, :order => [:id.asc]).should == items(4, 6)
-      Item.page(3, :per_page => 3, :order => [:id.asc]).should == items(7, 9)
-    end
-
-    it "should allow chaining of queries" do
-      Item.all(:id.lt => 4).page.should be_a(DataMapper::Collection)
+    
+    describe "option" do
+      describe ":per_page" do
+        it "should default to 6" do
+          Item.page.length.should == 6
+        end
+        
+        it "should be allow an alternate value" do
+          Item.page(1, :per_page => 3, :order => [:id.asc]).should == items(1, 3)
+          Item.page(2, :per_page => 3, :order => [:id.asc]).should == items(4, 6)
+          Item.page(3, :per_page => 3, :order => [:id.asc]).should == items(7, 9)
+        end
+        
+        it "should allow numeric strings" do
+          # Item.page(1, :per_page => '3', :order => [:id.asc]).should == items(1, 3)
+          # Item.page(2, :per_page => '3', :order => [:id.asc]).should == items(4, 6)
+          # Item.page(3, :per_page => '3', :order => [:id.asc]).should == items(7, 9)
+        end
+      end
+      
+      describe ":order" do
+        it "should default to [:id.desc]" do
+          Item.page.should == items(15, 20).reverse
+        end
+      end
+      
+      describe ":page" do
+        it "should raise an error unless responding to #to_i" do
+          lambda { Item.page(:page => true) }.should raise_error
+          lambda { Item.page('page' => true) }.should raise_error
+        end
+        
+         it "should delete keys when an indifferent hash is passed" do
+           Item.page(:page => '2', 'page' => '2', :order => [:id.asc]).should == items(7, 12)
+           Item.page(:page => '2', 'page' => '2', :order => [:id.asc]).pager.current_page.should == 2
+        end
+      end
     end
   end
   
