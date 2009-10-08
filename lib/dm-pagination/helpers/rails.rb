4eb1622d5c148b596cf0a40af47c9603c5c078be
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2009-10-08 15:53:17 +0800

    added docs for Helper::Rails

--- lib/dm-pagination/helpers/rails.rb
+++ lib/dm-pagination/helpers/rails.rb
@@ -1,7 +1,17 @@
 
 module DataMapper::Pagination::Helper::Rails
+
+  ##
+  # Renders the pagination links for the given _collection_.
+  #
+  # === Options
+  #
+  #   :params  Hash of params that is passed to url_for
+  #   :size    Number of intermediate page number links to be shown; Defaults to 7
+  
   def paginate collection, options = {}
     uri = @template.url_for @template.params.merge(options[:params] || {})
     collection.pager.to_html uri, options
   end
+
 end
